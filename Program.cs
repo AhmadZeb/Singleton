e7124e383bd281f2a77afca9c2b73efa5a74e416
Ahmed Zeb <ahmed.z@pacsquare.com>
2023-10-27 15:49:29 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //SingletonClass singletonClass = SingletonClass.GetInstance();
+            //singletonClass.PrintDetails("From Teacher");
+
+            //SingletonClass std = SingletonClass.GetInstance();
+            //std.PrintDetails("From Student");
+            //Console.WriteLine("Hello, World!");
+
+            //  Parallel.Invoke 
+            Parallel.Invoke(
+                //Let us Assume PrintTeacherDetails method is Invoked by Thread-1
+                () => PrintTeacherDetails(),
+                //Let us Assume PrintStudentDetails method is Invoked by Thread-2
+                () => PrintStudentDetails()
+                );
+            Console.ReadLine();
+        }
+
+        private static void PrintTeacherDetails()
+        {
+            //Thread-1 Calling the GetInstance() Method of the Singleton class
+            SingletonClass fromTeacher = SingletonClass.GetInstance();
+            fromTeacher.PrintDetails("From Teacher");
+        }
+
+        private static void PrintStudentDetails()
+        {
+            //At the same time, Thread-2 also Calling the GetInstance() Method of the Singleton Class
+            SingletonClass fromStudent = SingletonClass.GetInstance();
+            fromStudent.PrintDetails("From Student");
+        }
+    }
+ }
---
+++ SingletonClass.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class SingletonClass
+    {
+        private static int Counter = 0;
+
+        //This variable is going to store the Singleton Instance
+        private static SingletonClass Instance = null;
+        // Lock for single intastance 
+
+        // use with lock key word for create one object
+
+        //private static readonly object Instancelock  = new object();
+
+
+        //Non-Lazy or Eager Loading
+        //private static readonly SingletonClass singleInstance = new SingletonClass();
+
+        //Lazy Loading
+        private static readonly Lazy<SingletonClass> SingleInstance = new Lazy<SingletonClass>(() => new SingletonClass());
+        public static SingletonClass GetInstance()
+        {
+            //lock (Instancelock)
+            //{
+            //    if (instance == null)
+            //    {
+            //        instance = new SingletonClass();
+            //    }
+            //}
+            return SingleInstance.Value;
+        }
+        private SingletonClass()
+        {
+            Counter++;
+            Console.WriteLine("Counter Value " + Counter.ToString());
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
